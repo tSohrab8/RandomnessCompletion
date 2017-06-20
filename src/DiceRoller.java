84954953117f6b674ce5ca8fe16f89a753acae99
Taimur <tsohrab8@gmail.com>
2017-06-20 16:10:00 -0400

    initial commit

---
+++ src/DiceRoller.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+
+public class DiceRoller
+ {
+    public static void main( String[] args )
+    {
+        int dienumber;
+        Random rnd = new Random();
+        
+        dienumber = 1+ rnd.nextInt(6);
+        
+        System.out.println("Your die roll was : " + dienumber);
+    }
+ }
